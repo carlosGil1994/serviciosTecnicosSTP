58a9b984fdd9ba2dc0cd463e1b3c4011d80a9cd2
Carlos gil <carlosgilbonilla@gmail.com>
2018-08-14 22:23:29 -0400

    componentes inputs

--- app/Models/Cotizaciones.php
+++ app/Models/Cotizaciones.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cotizacion extends Model
+class Cotizaciones extends Model
 {
     //
 }
---
+++ resources/assets/js/components/inputs/select.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="form-group">
+        <label :for="forComputed">{{ etiquetaComputed }}</label>
+        <select>
+                <option disabled value="">Por favor escoja una opcion</option>
+                <option v-for="option in options" :key="option.id">{{option.name}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            options:{
+                type:Object,
+            },
+            for: {
+                type: String
+            },
+            etiqueta:{
+                type: String,
+                default: ''
+            },
+            clases:{
+                type: String
+            },
+            id:{
+                type: String
+            },
+            name:{
+                type: String,
+                required: true  
+            }
+        },
+        computed: {
+            idComputed() {
+                return this.id || this.name;
+            },
+            etiquetaComputed(){
+                return this.etiqueta ||  this.name.charAt(0).toUpperCase() + this.name.slice(1);
+            },
+            forComputed(){
+                return this.for || this.idComputed;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
